40f265c24ff865e37ee7009574af0e85b898a6c8
Andrea <malditoduende_metal2@hotmail.com>
2024-03-26 22:13:14 +0100

    subir ejercicios

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace ficheroBorrarLineas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce nombre del fichero: ");
+            string fichero = Console.ReadLine();
+
+            StreamReader f = File.OpenText(fichero);
+            string[] nuevoF = File.ReadAllLines(fichero);
+            List<string> lista = new List<string>(nuevoF);
+            string linea;
+            do
+            {
+                linea = f.ReadLine();
+                if (linea != null)
+                {
+                    if (linea == "") {
+                        lista.Remove(linea);
+                    }
+                }
+            } while (linea != null);
+
+            File.WriteAllLines("patata.txt", lista.ToArray());
+            f.Close();
+
+            //ejemplo iborra 
+
+            //Console.WriteLine("Introduce el nombre del fichero");
+            //string nombreEntrada = Console.ReadLine();
+            //StreamReader lectura = new StreamReader(nombreEntrada);
+            //StreamWriter escritura = new StreamWriter(nombreEntrada + ".sinVacios.txt");
+            //string lineas;
+            //do
+            //{
+            //    lineas = lectura.ReadLine();
+            //    if (lineas != null && lineas != "")
+            //    {
+            //        escritura.WriteLine(lineas);
+            //    }
+            //} while (lineas != null);
+            //lectura.Close();
+            //escritura.Close();
+
+        }
+    }
+}
+
